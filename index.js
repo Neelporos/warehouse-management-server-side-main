62d2169de24c81fb23556f7b9044876f900f1749
NeelPoros <sawkotalimahmud@gmail.com>
2022-05-07 02:39:48 -0700

    Connected With Mongodb

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+// 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zeg0g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('db connected');
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello World! Im Ready For Assignment 11')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`connected with mongodb ${port}`)
 })
